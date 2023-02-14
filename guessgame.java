b97817384fe0c0c6601d2eaf767e6b0a09855f86
MohammadAlhammad <118907926+MohammadAlhammad@users.noreply.github.com>
2023-02-14 19:30:54 +0300

    Update guessgame.java

--- guessgame.java
+++ guessgame.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 	    public static void main(String[] args) {
 	        
-	        Scanner =Mo = new Scanner(System.in);
+	        Scanner Mo = new Scanner(System.in);
 	        int[] numbers = {13, 7, 23, 2, 29, 3, 5, 71, 11, 41, 17, 19, 53, 73, 31, 97,43, 47, 59, 61, 79, 67, 89, 89};
 	        Arrays.sort(numbers);
 	        System.out.print("\n"+Arrays.toString(numbers)+"\n");
